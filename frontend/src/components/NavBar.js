adcf04030a065d0701cb366856e72f7d6b6ae769
Yirong <yw181920@gmail.com>
2024-02-23 20:27:05 -0600

    Placeholder logo, change to button on homepage

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,15 +1,16 @@
 // NavBar.js
 import React from 'react';
-import { AppBar, Toolbar, Button, Typography } from '@mui/material';
+import { AppBar, Toolbar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import logo from "../images/logo.png";
 
 function NavBar() {
   return (
     <AppBar position="static" sx={{ backgroundColor: '#00A3E0' }}>
       <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My Website
-        </Typography>
+      <Link to="/" style={{ flexGrow: 1 }}>
+          <img src={logo} alt="Logo" style={{ height: '50px' }} />
+        </Link>
         <Button color="inherit" component={Link} to="/lost-item">Lost Item</Button>
         <Button color="inherit" component={Link} to="/found-item">Found Item</Button>
       </Toolbar>
